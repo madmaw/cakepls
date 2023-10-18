7279bef784ce6b3a5e92e3723c735fd85a6a4b66
Chris Glover <chris.glover@gmail.com>
2023-10-18 13:30:25 +1100

    base

---
+++ src/ts/component/cake/input/base/component.tsx
@@ -0,0 +1,11 @@
+import type { CakeBaseType } from 'domain/model';
+
+export type CakeInputBaseProps = {
+  type: CakeBaseType,
+};
+
+export function CakeInputBase({
+  type,
+}: CakeInputBaseProps) {
+
+}
