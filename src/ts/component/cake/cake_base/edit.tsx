48ad26212533897ea1f67984cb21e60b6932f2ab
Chris Glover <chris.glover@gmail.com>
2023-12-01 13:24:09 +1100

    aggregate again

--- src/ts/component/cake/cake_base/edit.tsx
+++ src/ts/component/cake/cake_base/edit.tsx
@@ -1,9 +1,12 @@
 import { Stack } from '@mui/material';
+import type { ReactiveComponent } from 'base/component/reactive';
 import {
   adaptReactiveComponent,
   type ReactiveComponentProps,
+  useReactiveProps,
 } from 'base/component/reactive';
 import { UnreachableError } from 'base/errors';
+import type { ButterCakeBase } from 'domain/model';
 import {
   ButterCakeBaseType,
   type CakeBase,
@@ -13,15 +16,25 @@ import {
   SpongeCakeBaseType,
   WhiteCakeBaseType,
 } from 'domain/model';
-import { map } from 'rxjs';
+import { useMemo } from 'react';
+import type { Observable } from 'rxjs';
+import {
+  filter,
+  map,
+} from 'rxjs';
 
-import { EditCakeBaseSubtypeInCakeBase } from './edit_subtype';
+import type { EditCakeBaseSubtypeProps } from './subtype/edit';
+import { EditButterCakeBaseSubtype } from './subtype/edit';
 import type { EditCakeBaseTypeProps } from './type/edit';
 import { EditCakeBaseType } from './type/edit';
 
 export type EditCakeBaseProps = {
   readonly base: CakeBase,
 };
+
+//
+// type
+//
 
 function defaultCakeBase(type: CakeBaseType): CakeBase {
   switch (type) {
@@ -74,6 +87,89 @@ const EditCakeBaseTypeInCakeBase = adaptReactiveComponent<EditCakeBaseProps, Edi
     };
   }),
 );
+
+//
+// subtype
+//
+
+type EditCakeBaseSubtypeInCakeBaseProps<T extends CakeBaseType, S extends number> = {
+  readonly base: {
+    readonly type: T,
+    readonly subtype: S,
+  }
+};
+
+function createEditCakeBaseSubtypeInCakeBaseComponent<T extends CakeBaseType, S extends number>(
+    Component: ReactiveComponent<EditCakeBaseSubtypeProps<S>>,
+    type: T,
+) {
+  return adaptReactiveComponent<EditCakeBaseSubtypeInCakeBaseProps<T, S>, EditCakeBaseSubtypeProps<S>>(
+    Component,
+    map(function ({ base: { subtype } }: EditCakeBaseSubtypeInCakeBaseProps<T, S>) {
+      return {
+        value: subtype,
+      };
+    }),
+    map(function ([{ value }]: readonly [EditCakeBaseSubtypeProps<S>, EditCakeBaseSubtypeInCakeBaseProps<T, S>]) {
+      return {
+        base: {
+          type,
+          subtype: value,
+        },
+      };
+    }),
+  );
+}
+
+const EditButterCakeBaseSubtypeInCakeBase = createEditCakeBaseSubtypeInCakeBaseComponent(
+  EditButterCakeBaseSubtype,
+  CakeBaseType.Butter,
+);
+
+export function EditCakeBaseSubtypeInCakeBase({
+  props,
+  events,
+}: ReactiveComponentProps<{
+  readonly base: CakeBase,
+}>) {
+  // TODO this pattern feels suboptimal
+  const baseType = useReactiveProps(props)?.base.type;
+  const filteredProps = useMemo(function () {
+    return props.pipe(filter(function ({ base: { type } }) {
+      return type === baseType;
+    }));
+  }, [baseType, props]);
+
+  switch (baseType) {
+    case undefined:
+      return null;
+    case CakeBaseType.Butter:
+    {
+      return (
+        <EditButterCakeBaseSubtypeInCakeBase
+          // TODO remove cast somehow
+          // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+          props={filteredProps as Observable<{ readonly base: ButterCakeBase }>}
+          events={events}
+        />
+      );
+    }
+    case CakeBaseType.Carrot:
+    case CakeBaseType.Chocolate:
+    case CakeBaseType.Coffee:
+    case CakeBaseType.RedVelvet:
+    case CakeBaseType.Sponge:
+    case CakeBaseType.White:
+      // TODO: implement
+      return null;
+    default:
+      throw new UnreachableError(baseType);
+  }
+}
+
+//
+// edit
+//
 
 export function EditCakeBase(props: ReactiveComponentProps<EditCakeBaseProps>) {
   return (
